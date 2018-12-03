9a4ccb4192b30e83a0a1e410b3d6fa8fd922a685
Paul Thompson <pbt@pdx.edu>
2018-12-02 23:35:04 -0800

    Updating main

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='paul-numpy-example',
+    version='1',
+    packages=[''],
+    url='',
+    license='',
+    author='paul',
+    author_email='',
+    description=''
+)
